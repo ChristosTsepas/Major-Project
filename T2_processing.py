2a999b486ee960577f46e33ad1059ed39b8e72c6
ChristosTsepas <121814684+ChristosTsepas@users.noreply.github.com>
2026-02-23 13:32:36 +0100

    Update T2_processing.py

--- T2_processing.py
+++ T2_processing.py
@@ -4,10 +4,7 @@ Created on Thu Aug 31 13:49:09 2023
 
 @author: User
 """
-
 import nibabel as nib
-
-
 import numpy as np
 from dipy.align.reslice import reslice
 from dipy.io.image import load_nifti, save_nifti
@@ -25,76 +22,43 @@ def process_and_crop_T2(input_path, output_path):
     for filename in os.listdir(input_path):
         if filename.endswith(".nii"):
             file_path = os.path.join(input_path, filename)
-            
             data, affine, coord = load_nifti(file_path, return_coords=True)
-            
-            #mins = [25,40,0]
-            
-            #maxs = [103,90,20]
-            
             ###  for T2 change here
             mins = [1, 15, 0]
-            
             maxs = [87, 71, 14]
-            
             cropped_data = crop(data, mins, maxs)
-            
             # Create the output file path in the output directory
             output_file_path = os.path.join(output_path, filename.replace("_resampled.nii", ".nii"))
-            
             # Create a NIfTI image with the cropped data
             cropped_nii = nib.Nifti1Image(cropped_data, affine)
-            
             # Save the cropped T2 image to the output file path
             nib.save(cropped_nii, output_file_path)
 
-
-def check_correct_negative_values(input_path, output_path):
-
-       
-        
+def check_correct_negative_values(input_path, output_path):        
         for filename in os.listdir(input_path):
-            
             if filename.endswith('.nii'):
-                
                 t2_array, affine = load_nifti(os.path.join(input_path, filename))
-                
                 basename = filename.replace('.nii', '')
-
                 if np.any(t2_array < 0):
                     print(f"There are negative values in the {basename} volume.")
-                    
                     t2_array[t2_array < 0] = 0
-                    
                 else:
                     print(f"No negative values found in the {basename} volume.")
-                    
-                    
-        
-                
+                     
                 output_filename = f"{basename}.nii"
-        
-        
                 nii_img = nib.Nifti1Image(t2_array, affine)
                 # Save the NIfTI file
                 nib.save(nii_img, os.path.join(output_path, output_filename))
-        
                 print(f"Modified volume saved to: {os.path.join(output_path, output_filename)}")
 
 if __name__ == "__main__":
     # Set your input directory containing the .nii files
     input_directory = r"H:\Marc_Pastur_pipeline\T2 processing\Resampled and cropped"
-    
     # Set your output directory for saving the cropped .nii files
     output_directory = r"H:\Marc_Pastur_pipeline\T2 processing\Correct for negative values"
-    
     check_correct_negative_values(input_directory, output_directory)
-    
     # Process and crop all .nii files in the input directory
     process_and_crop_T2(input_directory, output_directory)
-    
-    
-    
 
 
 
@@ -192,5 +156,6 @@ nib.save(t2_img,'full_t2_masked.nii')
 
 
 #save_nifti('t2_1mmresliced.nii', t2_data, t2_affine2)
+
 
 '''
